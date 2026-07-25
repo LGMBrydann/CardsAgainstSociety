cb192ec552099aebbce820b80a1ecd614f2cd910
LGMBrydann <itslgmbrydan@gmail.com>
2026-07-25 16:42:43 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRtNVJR6ejJ2dGxXOr9uWNpef7NfrYyv8",
+  authDomain: "cardsagainstsociety-auth.firebaseapp.com",
+  projectId: "cardsagainstsociety-auth",
+  storageBucket: "cardsagainstsociety-auth.firebasestorage.app",
+  messagingSenderId: "370760597907",
+  appId: "1:370760597907:web:0f7dd22f21ac8ec331a866",
+  measurementId: "G-HKZ70SNLXE"
+};
